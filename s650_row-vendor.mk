96a429f9905e05e95647b1e9e1968cf155f0261a
kyryljan-serhij <kirilyan.sergei@gmail.com>
2016-01-10 02:58:14 +0200

    change for Lenovo S650 ROW

---
+++ s650_row-vendor.mk
@@ -0,0 +1,10 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/s650_row/bin,system/bin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/s650_row/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/s650_row/etc,system/etc)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/s650_row/vendor,system/vendor)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/lenovo/s650_row/xbin,system/xbin)
+
